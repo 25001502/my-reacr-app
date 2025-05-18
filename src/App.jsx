59e386cf8ac5f45a39c92f84ac8b427f1aef4b6c
25001502 <25001502@mvula.univen.ac.za>
2025-05-18 23:19:02 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from './Header.jsx';
 import Button from './Button.jsx';
-import Image from './image.jsx';
+import Image from './Image.jsx';
 import Footer from './Footer.jsx';
 
 function App() {
